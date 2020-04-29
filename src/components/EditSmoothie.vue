63344e17047952600c43ad4104a16e3e0537fd01
Mohit Chandana <chandanamohit@gmail.com>
2020-04-30 03:52:11 +0530

    Create edit smoothie router and component

---
+++ src/components/EditSmoothie.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="edit-smoothie container">
+    <h2>Edit Smoothie</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EditSmoothie',
+  data(){
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
