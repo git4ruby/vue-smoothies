bed33660ad9490f3b9d252203d442b3b16fe1bcd
Mohit Chandana <chandanamohit@gmail.com>
2020-04-30 02:42:59 +0530

    Create add smoothie component and create route

---
+++ src/components/AddSmoothie.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>Add Smoothie</h2>
+</template>
+
+<script>
+export default {
+  name: 'AddSmoothie',
+  data(){
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import AddSmoothie from '@/components/AddSmoothie'
 
 Vue.use(VueRouter)
 
   const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'Index',
     component: Home
   },
   {
@@ -17,6 +18,11 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/add-smoothie',
+    name: 'AddSmoothie',
+    component: AddSmoothie
   }
 ]
 
